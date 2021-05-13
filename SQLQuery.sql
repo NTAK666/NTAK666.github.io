7d135b0d1d9e8c375b37f49beecc6b2393a4dd9f
NTAK666 <anhkiet8686@gmail.com>
2021-05-14 06:32:41 +0700

    123

---
+++ SQLQuery.sql
@@ -0,0 +1,16 @@
+CREATE TABLE dbo.GiaoVien
+(
+    IDGV INT NOT NULL PRIMARY KEY,
+    -- primary key column
+    UserName [NVARCHAR](50) NOT NULL,
+    Password [NVARCHAR](50) NOT NULL
+);
+GO
+CREATE TABLE dbo.GiaoVien
+(
+    IDGV INT NOT NULL PRIMARY KEY,
+    -- primary key column
+    UserName [NVARCHAR](50) NOT NULL,
+    Password [NVARCHAR](50) NOT NULL
+);
+GO
