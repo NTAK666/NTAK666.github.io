a403a9b6a346e7359b2f62fc81ca20b25bda1a62
NTAK666 <anhkiet8686@gmail.com>
2021-05-24 08:27:13 +0700

    123

--- Example-PrimKey$nXuIBaeHa6/240521/ex2/hover.js
+++
@@ -1,7 +0,0 @@
-function hover(element) {
-    element.setAttribute('src', 'https://assets.adidas.com/images/w_600,f_auto,q_auto/92d1cd7570cd4aea8c5bacd1012207dd_9366/Quan_Short_The_Thao_3_Soc_Vai_Det_Primeblue_Designed_2_Move_DJo_GN3108_21_model.jpg');
-}
-
-function unhover(element) {
-    element.setAttribute('src', 'https://assets.adidas.com/images/w_600,f_auto,q_auto/ae613c03b5334383888eac690141f182_9366/Quan_Short_The_Thao_3_Soc_Vai_Det_Primeblue_Designed_2_Move_DJen_GL3981_21_model.jpg');
-}
